35f1d73a1b1dacdd9a3106acef2154f33ca38de6
dexSamir <samirah-bp215@code.edu.az>
2024-10-29 11:34:19 +0400

    create Hotel and Room classes and connect each other and add some methods in the end create console app

---
+++ Example/Core/Data/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Core.Model;
+
+namespace Core.Data
+{
+    public static class AppDbContext
+    {
+        private static List<Hotel> hotels = new List<Hotel>();
+        public static void AddHotel(Hotel hotel)
+        {
+            hotels.Add(hotel);
+        }
+
+        public static void ShowAllHotels()
+        {
+            foreach (var hotel in hotels)
+            {
+                hotel.ShowInfo();
+                Console.WriteLine();
+            }
+        }
+        public static Hotel findHotel(string name)
+        {
+            name.Trim().ToLower(); 
+            foreach (var hotel in hotels)
+            {
+                if(hotel.Name == name)
+                {
+                    return hotel; 
+                }
+            }
+            return null; 
+        }
+    }
+}
+
---
+++ Example/Core/Model/Room.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Core.Model
+{
+    public class Room
+    {
+        private static int _idCounter = 1;
+        public int Id { get; private set; }
+        public string No { get; private set; }
+        public int PersonCapacity { get; private set; }
+
+        public Room(string no, int personCapacity)
+        {
+            Id = _idCounter++;
+            No = no;
+            PersonCapacity = personCapacity;
+        }
+
+        public override string ToString()
+        {
+            return $"Room ID: {Id}, Room No: {No}, Person Capacity: {PersonCapacity}";
+        }
+    }
+}
+
